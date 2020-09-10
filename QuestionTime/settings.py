0efb31549399ee594e02e6f7acc488f1511d5b4c
Erick-ViBe <erickvb12@gmail.com>
2020-09-10 13:30:51 -0500

    UP

--- QuestionTime/settings.py
+++ QuestionTime/settings.py
@@ -142,7 +142,7 @@ LOGOUT_REDIRECT_URL = "/"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
