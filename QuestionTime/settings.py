95d7b335775555d01de0782d9e5c66602a942246
Erick-ViBe <erickvb12@gmail.com>
2021-03-14 14:44:12 -0600

    Trying to run in Heroku

--- QuestionTime/settings.py
+++ QuestionTime/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -201,3 +203,5 @@ WEBPACK_LOADER = {
         'STATS_FILE': os.path.join(BASE_DIR, 'frontend', 'webpack-stats.json'),
     }
 }
+
+django_heroku.settings(locals())
